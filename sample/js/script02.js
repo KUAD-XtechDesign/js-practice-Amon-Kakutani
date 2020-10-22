bd6a28a6b01cd05cae4d8dbba385d39452a1b263
Amon-Kakutani <68629390+Amon-Kakutani@users.noreply.github.com>
2020-10-22 20:01:42 +0900

    変更

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,6 +1,8 @@
 $(function(){
 let txt1="おはよう";
 let txt2="愛してる";
+let txt3="愛してる。。。あの世でも。。。";
+let txt4="RIP";
 let num1=29;
 let num2=3;
 let num3="9余り2";
@@ -14,7 +16,10 @@ $("#button02").on("click",function(){
     $("#content").append(txt2);
 })
 $("#button03").on("click",function(){
-    $("#content").text(txt1+txt2);
+    $("#content").text(txt3);
+})
+$("#button04").on("click",function(){
+    $("#content").text(txt4);
 })
 $("#button04").on("click",function(){
     $("#content").text(num1+num2);
