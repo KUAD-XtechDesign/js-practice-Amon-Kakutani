cbefbd6906e25314ecc14f6553d678485e9cca5a
Amon-Kakutani <68629390+Amon-Kakutani@users.noreply.github.com>
2020-10-22 19:30:42 +0900

    変更

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -6,21 +6,21 @@ let num2=3;
 let num3="9余り2";
 
 $("#button01").on("click",function(){
-    $("#content").text("ヤッホー");
+    $("#content").append("ヤッホー");
 })
 
 $("#button02").on("click",function(){
-    $("#content").text(txt1);
+    $("#content").append(txt1);
 })
 $("#button03").on("click",function(){
-    $("#content").text(txt1+txt2);
+    $("#content").append(txt1+txt2);
 })
 $("#button04").on("click",function(){
-    $("#content").text(num1+num2);
+    $("#content").append(num1+num2);
 })
 
 $("#button05").on("click",function(){
-    $("#content").text(num1+"÷"+num2+"="+num3+"です");
+    $("#content").append(num1+"÷"+num2+"="+num3+"やで");
 })
 
 });
