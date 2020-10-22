87073a81115c20992b97d89e4db6b55f22674076
Amon-Kakutani <68629390+Amon-Kakutani@users.noreply.github.com>
2020-10-22 19:41:26 +0900

    henkou

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -5,22 +5,23 @@ let num1=29;
 let num2=3;
 let num3="9余り2";
 
+
 $("#button01").on("click",function(){
     $("#content").extend("ヤッホー");
 })
 
 $("#button02").on("click",function(){
-    $("#content").extend(txt1);
+    $("#content").text(txt1);
 })
 $("#button03").on("click",function(){
-    $("#content").extend(txt1+txt2);
+    $("#content").text(txt1+txt2);
 })
 $("#button04").on("click",function(){
-    $("#content").extend(num1+num2);
+    $("#content").text(num1+num2);
 })
 
 $("#button05").on("click",function(){
-    $("#content").extend(num1+"÷"+num2+"="+num3+"やで");
+    $("#content").text(num1+"÷"+num2+"="+num3+"やで");
 })
 
 });
